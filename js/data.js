29e3b745f2abc88196262d40de4f94c50ff59f38
rattofix <alefoto957@gmail.com>
2025-09-22 15:51:59 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,29 +3,28 @@ const groupsData = {
         title: "Gruppo Medie",
         subtitle: "Scuola Secondaria di Primo Grado",
         schedule: "Domenica 15:00-17:00",
-        description: "Un percorso di crescita e divertimento per i ragazzi delle scuole medie. Attività formative, giochi e momenti di condivisione per crescere insieme.",
+        description: "Il gruppo medie è uno spazio riservato ai ragazzi che vogliono intraprendere un percorso ricco di svago e divertimento, accompagnato anche da momenti favorevoli alla crescita personale e dunque volti alla riflessione. Il gruppo si riunisce non solo la domenica, ma anche il venerdì: a partire dalle 14, i ragazzi hanno la possibilità di pranzare e fare i compiti insieme a noi educatori. Grazie a questo gruppo, i ragazzi hanno l’occasione di stringere nuovi legami con i propri coetanei e di confrontarsi con noi educatori, più grandi, su temi di cui sentono l’esigenza di parlare. Poiché il gruppo nasce in un contesto parrocchiale, accompagneremo i ragazzi anche nel loro percorso di fede.",
         activities: [
-            "Incontri settimanali di formazione",
+            "Incontri settimanali la domenica",
             "Attività ricreative e giochi di gruppo",
-            "Uscite e gite formative",
-            "Momenti di preghiera condivisi"
+            "Pranzi comunitari il venerdì seguiti da studio insieme e giochi"
         ],
         image: "../medie.jpg",
-        contactPerson: "Referente: Marco Rossi - 333 123 4567"
+        contactPerson: "Per info: Valentino Remedi - 371 314 5782"
     },
     adolescenti: {
         title: "Gruppo Adolescenti",
         subtitle: "Scuola Secondaria di Secondo Grado",
         schedule: "Domenica 17:45-19:30",
-        description: "Il gruppo adolescenti è un occasione per i ragazzi di intraprendere un percorso educativo ricco di esperienze; infatti durante gli incontri domenicali avranno modo di riflettere su sé stessi e su questioni che li riguardano da vicino; inoltre non mancheranno momenti di svago e di divertimento, necessari per consolidare i legami interpersonali tra di loro e con gli educatori. Poiché il gruppo educativo si svolge all’interno di un contesto parrocchiale, il nostro orizzonte di senso sarà orientato innanzitutto dalla ricerca della fede, il nostro filo rosso durante tutto l’anno pastorale. Il GA non si riduce esclusivamente agli incontri della domenica: ci saranno altri momenti durante la settimana e l’anno nei quali avremo modo di fare nuove esperienze con loro, perché crediamo che un percorso educativo serio richieda tempo, affinché l’atto educativo abbia un’efficacia reale.",
+        description: "Il gruppo adolescenti è un'occasione per i ragazzi di intraprendere un percorso educativo ricco di esperienze; infatti durante gli incontri domenicali avranno modo di riflettere su sé stessi e su questioni che li riguardano da vicino; inoltre non mancheranno momenti di svago e di divertimento, necessari per consolidare i legami interpersonali tra di loro e con gli educatori. Poiché il gruppo educativo si svolge all’interno di un contesto parrocchiale, il nostro orizzonte di senso sarà orientato innanzitutto dalla ricerca della fede, il nostro filo rosso durante tutto l’anno pastorale. Il GA non si riduce esclusivamente agli incontri della domenica: ci saranno altri momenti durante la settimana (come gli incontri del venerdì pomeriggio) e l’anno nei quali avremo modo di fare nuove esperienze con loro, perché crediamo che un percorso educativo serio richieda tempo, affinché l’atto educativo abbia un’efficacia reale.",
         activities: [
-            "Incontri di formazione e confronto",
+            "Incontri settimanali la domenica",
             "Attività di volontariato",
-            "Serate a tema e uscite",
-            "Percorsi di fede personalizzati"
+            "Attività a tema e uscite",
+            "Pomeriggi di studio insieme e giochi durante la settimana"
         ],
         image: "../ado.jpg",
-        contactPerson: "Referente: Anna Verdi - 333 765 4321"
+        contactPerson: "Per info: Mattia Saccon - 370 346 1798"
     },
     giovani: {
         title: "Gruppo Giovani",
@@ -52,6 +51,6 @@ const groupsData = {
             "Incontri di spiritualità e preghiera",
             "Momenti di convivialità e festa"
         ],
-        contactPerson: "Referenti: Luca Bianchi e Sara Neri - 333 555 7890"
+        contactPerson: "Per info: Elena Manzoni "
     }
 };
